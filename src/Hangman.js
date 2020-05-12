1c885174f8405a3822dfac0f3ac95528de7de836
RoboVij <robovij83@gmail.com>
2020-05-12 13:39:26 +0200

    New version2

--- src/Hangman.js
+++ src/Hangman.js
@@ -83,7 +83,7 @@ class Hangman extends Component {
         <p className='Hangman-btns'>
           {gameState}
         </p>
-        <button onClick={this.restart}>Restart</button>
+        <button onClick={this.restart}>Restart EVerything</button>
       </div>
     );
   }
